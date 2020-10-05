d97533226ab57f191bcc4220dffd6ace7b46ca5a
Counterdevel <pierreeitel@hotmail.com>
2020-10-05 17:57:56 -0300

    Stranger Farm
    Movimento astronauta e cinemachine

---
+++ Stranger Farm/Assets/Scripts/AstronautaMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronautaMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 5f;
+
+    public float turnsSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude > 0.1)
+        {
+            float angleDirection = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angleDirection, ref turnSmoothVelocity, turnsSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
+ 
