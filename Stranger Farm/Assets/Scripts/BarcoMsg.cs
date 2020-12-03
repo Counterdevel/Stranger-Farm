ea6f19724e2fef341f7fea706e02a78e60342aa8
Counterdevel <pierreeitel@hotmail.com>
2020-12-03 17:20:33 -0300

    Cercas, animação antena, concertado plantas, barco mostra mensagem

---
+++ Stranger Farm/Assets/Scripts/BarcoMsg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarcoMsg : MonoBehaviour
+{
+    public Text junteMoney;
+
+    private void Start()
+    {
+        junteMoney.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            print("djfgosfj");
+            junteMoney.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            junteMoney.enabled = false;
+        }
+    }
+}
